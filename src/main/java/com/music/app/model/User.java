a2409a1be2212c1ea3cdd08732b609914f439e2c
Khanh-Nobi-JR <69251285+Khanh-Nobi-JR@users.noreply.github.com>
2021-01-10 22:48:22 +0700

    first commit

---
+++ src/main/java/com/music/app/model/User.java
@@ -0,0 +1,20 @@
+package com.music.app.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String username;
+    private String password;
+    private String email;
+    @ManyToMany
+    private Set<Role> roles;
+
+}
