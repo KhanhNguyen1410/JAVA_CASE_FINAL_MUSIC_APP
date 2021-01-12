d42f8eead22c6c54ef1da1ff406ff68ac53cbe20
Khanh-Nobi-JR <69251285+Khanh-Nobi-JR@users.noreply.github.com>
2021-01-12 14:17:26 +0700

    add jwt service add component

--- src/main/java/com/music/app/service/jwt/JwtService.java
+++ src/main/java/com/music/app/service/jwt/JwtService.java
@@ -1,5 +1,8 @@
 package com.music.app.service.jwt;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class JwtService {
     private static final String SECRET_KEY =  ("Avengers");
     private static final Long EXPIRE_TIME = 100000000L;
