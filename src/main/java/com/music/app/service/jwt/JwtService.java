4e5f22fbd1de2fe1c66fd51e856e0f9fd2e1fe99
Khanh-Nobi-JR <69251285+Khanh-Nobi-JR@users.noreply.github.com>
2021-01-12 14:02:36 +0700

    add jwt service

---
+++ src/main/java/com/music/app/service/jwt/JwtService.java
@@ -0,0 +1,6 @@
+package com.music.app.service.jwt;
+
+public class JwtService {
+    private static final String SECRET_KEY =  ("Avengers");
+    private static final Long EXPIRE_TIME = 100000000L;
+}
